2ab19f1494fee492b9fcfc0cef8c80f7cace6523
joao <jovmamikl@gmail.com>
2026-05-08 09:34:09 -0300

    feat: commit inicial projeto para url encurtada

---
+++ src/main/java/app/joaovirone/urlencurtador/UrlencurtadorApplication.java
@@ -0,0 +1,13 @@
+package app.joaovirone.urlencurtador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlencurtadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlencurtadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/joaovirone/urlencurtador/UrlencurtadorApplicationTests.java
@@ -0,0 +1,13 @@
+package app.joaovirone.urlencurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlencurtadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
